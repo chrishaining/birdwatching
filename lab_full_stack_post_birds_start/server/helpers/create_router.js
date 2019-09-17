ee06149f1d25fdf919c7d2df1b97b33b3ad3d2eb
Chris Haining <chrishaining@yahoo.co.uk>
2019-09-17 15:27:14 +0100

    adds backend delete functionality.

--- lab_full_stack_post_birds_start/server/helpers/create_router.js
+++ lab_full_stack_post_birds_start/server/helpers/create_router.js
@@ -43,9 +43,18 @@ const createRouter = function (collection) {
     });
   });
 
-//   delete method
-// router.delete('/:id', (req, res) )
-
+  // delete method
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    collection
+    .deleteOne( { _id: ObjectID(id) })
+    .then(result => {res.json(result)})
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
 
   return router;
 };
